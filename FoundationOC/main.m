8f5fde7791ca1b533fea17c1ca154f2c3351c356
jiliuliu <liujiliu1989@163.com>
2017-08-16 23:07:02 +0800

    Initial Commit

---
+++ FoundationOC/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  FoundationOC
+//
+//  Created by 刘吉六 on 2017/8/16.
+//  Copyright © 2017年 liujiliu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
